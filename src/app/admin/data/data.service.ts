855025c27409d723085da5e7ac25e98213f57ac6
anas <anasatwork01@gmail.com>
2024-01-27 01:57:43 +0500

    complete

--- src/app/admin/data/data.service.ts
+++ src/app/admin/data/data.service.ts
@@ -99,5 +99,160 @@ export class DataService {
 
       return data;
     }
+    if (type === GRAPH_TYPE.MONTH) {
+      const date = new Date();
+      const firstDay = new Date(date.getFullYear(), 0, 1);
+      let pipeline = [
+        {
+          $match: {
+            created_at: {
+              $gte: firstDay,
+            },
+          },
+        },
+
+        {
+          $group: {
+            _id: {
+              year: { $year: '$created_at' },
+              month: { $month: '$created_at' },
+            },
+            aqi: { $avg: '$aqi' },
+            dust: { $avg: '$dust' },
+            co2: { $avg: '$co2' },
+            co: { $avg: '$co' },
+            no2: { $avg: '$no2' },
+            nh3: { $avg: '$nh3' },
+            ch4: { $avg: '$ch4' },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            date: {
+              day: '$_id.day',
+              month: '$_id.month',
+              year: '$_id.year',
+            },
+            aqi: 1,
+            dust: 1,
+            co2: 1,
+            co: 1,
+            no2: 1,
+            nh3: 1,
+            ch4: 1,
+          },
+        },
+        {
+          $sort: { 'date.day': 1 },
+        },
+      ];
+
+      const readings = await this.dataRepository.aggregate(pipeline).toArray();
+      const aqiArr = [];
+      const dustArr = [];
+      const co2Arr = [];
+      const coArr = [];
+      const no2Arr = [];
+      const nh3Arr = [];
+      const ch4Arr = [];
+      const labels = [];
+
+      for (let i = 0; i < readings.length; i++) {
+        aqiArr.push(readings[i].aqi);
+        dustArr.push(readings[i].dust);
+        co2Arr.push(readings[i].co2);
+        coArr.push(readings[i].co);
+        no2Arr.push(readings[i].no2);
+        nh3Arr.push(readings[i].nh3);
+        ch4Arr.push(readings[i].ch4);
+        labels.push(readings[i].date);
+      }
+
+      const data = {
+        aqi: aqiArr,
+        dust: dustArr,
+        co2: co2Arr,
+        co: coArr,
+        no2: no2Arr,
+        nh3: nh3Arr,
+        ch4: ch4Arr,
+        labels,
+      };
+
+      return data;
+    }
+    if (type == GRAPH_TYPE.YEAR) {
+      let pipeline = [
+        {
+          $group: {
+            _id: {
+              year: { $year: '$created_at' },
+            },
+            aqi: { $avg: '$aqi' },
+            dust: { $avg: '$dust' },
+            co2: { $avg: '$co2' },
+            co: { $avg: '$co' },
+            no2: { $avg: '$no2' },
+            nh3: { $avg: '$nh3' },
+            ch4: { $avg: '$ch4' },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            date: {
+              day: '$_id.day',
+              month: '$_id.month',
+              year: '$_id.year',
+            },
+            aqi: 1,
+            dust: 1,
+            co2: 1,
+            co: 1,
+            no2: 1,
+            nh3: 1,
+            ch4: 1,
+          },
+        },
+        {
+          $sort: { 'date.day': 1 },
+        },
+      ];
+
+      const readings = await this.dataRepository.aggregate(pipeline).toArray();
+      const aqiArr = [];
+      const dustArr = [];
+      const co2Arr = [];
+      const coArr = [];
+      const no2Arr = [];
+      const nh3Arr = [];
+      const ch4Arr = [];
+      const labels = [];
+
+      for (let i = 0; i < readings.length; i++) {
+        aqiArr.push(readings[i].aqi);
+        dustArr.push(readings[i].dust);
+        co2Arr.push(readings[i].co2);
+        coArr.push(readings[i].co);
+        no2Arr.push(readings[i].no2);
+        nh3Arr.push(readings[i].nh3);
+        ch4Arr.push(readings[i].ch4);
+        labels.push(readings[i].date);
+      }
+
+      const data = {
+        aqi: aqiArr,
+        dust: dustArr,
+        co2: co2Arr,
+        co: coArr,
+        no2: no2Arr,
+        nh3: nh3Arr,
+        ch4: ch4Arr,
+        labels,
+      };
+
+      return data;
+    }
   }
 }
